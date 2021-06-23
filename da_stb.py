d252f345d72b9cb02de716cd80e1c761e2ee4c95
Pim Meulensteen <pimmeulensteen+git@gmail.com>
2021-06-23 14:11:14 +0200

    Finished dastb

--- da_stb.py
+++ da_stb.py
@@ -1,1 +1,18 @@
-from random import shuffle
+from typing import List
+from random import sample
+
+
+def da_stb(schools: List[int], students: List[List[int]]) -> List[int]:
+    used = [0] * len(schools)
+    placement = []
+    for student in sample(students, len(students)):
+        placed = False
+        for rank, school_at_rank in enumerate(student):
+            if used[school_at_rank] < schools[school_at_rank]:
+                placement.append(rank + 1)
+                used[school_at_rank] += 1
+                placed = True
+                break
+        if not placed:
+            placement.append(13)
+    return placement
